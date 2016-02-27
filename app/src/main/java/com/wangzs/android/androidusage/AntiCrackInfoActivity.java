3ddd8321229cc5aa0c40776014f9d7b8fdcd5246
wangzs <zhenshui1989@sina.com>
2016-02-27 15:55:50 +0800

    修改：自定义ToolBar

--- app/src/main/java/com/wangzs/android/androidusage/AntiCrackInfoActivity.java
+++ app/src/main/java/com/wangzs/android/androidusage/AntiCrackInfoActivity.java
@@ -3,6 +3,8 @@ package com.wangzs.android.androidusage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.wangzs.android.anticrack.AntiCrackTools;
@@ -11,6 +13,8 @@ import java.io.IOException;
 
 public class AntiCrackInfoActivity extends AppCompatActivity {
     private TextView anti_crack_info_tv;
+    private ImageButton back_btn;
+    private TextView toolbar_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +26,23 @@ public class AntiCrackInfoActivity extends AppCompatActivity {
 
     private void initUI() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("AntiCrack");    // need use this change toolbar title
-        //toolbar.setTitle("AntiCrack");
+//        getSupportActionBar().setTitle("AntiCrack");    // need use this change toolbar title
+
+        toolbar_title = (TextView) findViewById(R.id.toolbar_title);
+        toolbar_title.setText("AntiCrack");
 
         anti_crack_info_tv = (TextView)findViewById(R.id.anti_crack_info_tv);
         anti_crack_info_tv.setText(getAntiCrackInfo());
+
+        back_btn = (ImageButton) findViewById(R.id.back_btn);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     private String getAntiCrackInfo() {
